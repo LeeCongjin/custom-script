85e6480557ea36115e95933c47aec82f5b0011de
congjin.li <congjin.li@tusen.ai>
2023-08-15 18:21:38 +0800

    fix: 修改通用模版

--- template/modal/TemplateModal.vue
+++ template/modal/TemplateModal.vue
@@ -1,67 +1,49 @@
 <template>
-	<WayzardModal
-		class="modalClassName"
-		v-model:value="isShowModal"
-		:title="$t('测试弹窗')"
-		:show-header-divider="false"
-		:show-footer-divider="false"
-		:modal-props="{
-			closeOnEsc: false,
-		}"
-		:closeable="!loading"
-		:content-style="{
-			width: 600 + 'px',
-		}"
-		:mask-closable="false"
-		@after-leave="handleClose"
-	>
-		<div>
-			<n-form ref="formRef" :rules="rules" :model="formData">
-				<n-form-item
-					path="version"
-					:label="$t('标签')"
-					:show-require-mark="false"
-				>
-					<n-input
-						v-model:value="formData.inputValue"
-						:disabled="loading"
-						:placeholder="$t('请输入xxx')"
-						clearable
-						type="text"
-					/>
-				</n-form-item>
-			</n-form>
-		</div>
-		<template #footer>
-			<div style="display: flex; justify-content: flex-end; gap: 16px">
-				<n-button :disabled="loading" size="small" round @click="handleCancel">
-					{{ $t("取消") }}
-				</n-button>
-				<n-button
-					size="small"
-					round
-					type="primary"
-					:loading="loading"
-					:disabled="loading"
-					@click="handleConfirm"
-				>
-					{{ $t("确定") }}
-				</n-button>
-			</div>
-		</template>
-	</WayzardModal>
+  <n-modal
+    class="modalClassName"
+    v-model:show="isShowModal"
+    preset="dialog"
+    @after-leave="handleClose"
+  >
+    <div>
+      <n-form ref="formRef" :rules="rules" :model="formData">
+        <n-form-item path="version" label="标签" :show-require-mark="false">
+          <n-input
+            v-model:value="formData.inputValue"
+            :disabled="loading"
+            clearable
+            type="text"
+          />
+        </n-form-item>
+      </n-form>
+    </div>
+    <template #action>
+      <div style="display: flex; justify-content: flex-end; gap: 16px">
+        <n-button :disabled="loading" size="small" round @click="handleCancel">
+          取消
+        </n-button>
+        <n-button
+          size="small"
+          round
+          type="primary"
+          :loading="loading"
+          :disabled="loading"
+          @click="handleConfirm"
+        >
+          确定
+        </n-button>
+      </div>
+    </template>
+  </n-modal>
 </template>
 
 <script setup lang="ts">
-import WayzardModal from "@/components/common/WayzardModal.vue"
-import { defineProps, defineEmits, ref, reactive, computed, watch } from "vue"
+import { ref, reactive, computed, watch } from "vue"
 import { useVModel } from "@vueuse/core"
 import { FormRules } from "naive-ui"
 interface ModalProps {
-	show: boolean
-	deviceName: string
-	deviceType: string
-	deviceVersion: string
+  show: boolean
+  inputValue: string
 }
 
 // 中间区域不要修改
@@ -74,64 +56,64 @@ const isShowModal = useVModel(props, "show")
 const rules: FormRules = []
 
 const formData = reactive({
-	inputValue: props.deviceName,
+  inputValue: props.inputValue,
 })
 
 const callBackData = computed(() => {
-	return {
-		formData,
-	}
+  return {
+    formData,
+  }
 })
 watch(
-	() => props.show,
-	() => {
-		if (props.show) {
-			formData.inputValue = props.deviceName
-		} else {
-			formData.inputValue = ""
-		}
-	}
+  () => props.show,
+  () => {
+    if (props.show) {
+      formData.inputValue = props.inputValue
+    } else {
+      formData.inputValue = ""
+    }
+  }
 )
 
 const emits = defineEmits<{
-	(e: "update:show", value: boolean): void
-	(e: "close", param: typeof callBackData.value): void
-	(
-		e: "confirm",
-		param: typeof callBackData.value,
-		close: () => void,
-		endLoading: () => void
-	): void
-	(e: "cancel", param: typeof callBackData.value): void
+  (e: "update:show", value: boolean): void
+  (e: "close", param: typeof callBackData.value): void
+  (
+    e: "confirm",
+    param: typeof callBackData.value,
+    close: () => void,
+    endLoading: () => void
+  ): void
+  (e: "cancel", param: typeof callBackData.value): void
 }>()
 function handleCancel() {
-	// 中间区域不要修改
-	emits("cancel", callBackData.value)
-	isShowModal.value = false
-	// 中间区域不要修改
+  // 中间区域不要修改
+  emits("cancel", { ...callBackData.value })
+  isShowModal.value = false
+  // 中间区域不要修改
 }
 
 function handleClose() {
-	// 中间区域不要修改
-	emits("close", callBackData.value)
-	isShowModal.value = false
-	// 中间区域不要修改
+  // 中间区域不要修改
+  emits("close", { ...callBackData.value })
+  isShowModal.value = false
+  // 中间区域不要修改
 }
 
 function handleConfirm() {
-	// 中间区域不要修改
-	loading.value = true
-	emits(
-		"confirm",
-		callBackData.value,
-		() => {
-			loading.value = false
-			isShowModal.value = false
-		},
-		() => {
-			loading.value = false
-		}
-	)
-	// 中间区域不要修改
+  // 中间区域不要修改
+  loading.value = true
+  emits(
+    "confirm",
+    { ...callBackData.value },
+    () => {
+      loading.value = false
+      isShowModal.value = false
+    },
+    () => {
+      loading.value = false
+    }
+  )
+  // 中间区域不要修改
 }
 </script>
